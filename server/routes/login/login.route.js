3fd031e89ce0d2ab709218fe7343d4e48052be80
lorang92 <Steffen.ekeberg@gmail.com>
2016-03-16 23:35:07 +0100

    minor changes to login-router

--- server/routes/login/login.route.js
+++ server/routes/login/login.route.js
@@ -13,37 +13,11 @@ var User = require('../../models/user/user.model');
 module.exports = function(app){
 
   router.post('/login', auth, function(req, res, next) {
-    if (req.user) {
-    	//User successfully authenticated
-    	res.status(200);
-    	res.send({status: 200});
-    }
-    
-  });
-
-  
-  router.post('/login', auth, function(req, res,next){
-  		if(req.user){
-  			res.status(200); 
-  			res.send({status: 200}); 		}
-  });
-
-
-  router.get('/init',function(req,res){
-  	var tore = new User({
-  		username: 'tore',
-  		email: 'tore@tore.no',
-  		password: 'tore'
-  	});
-  	tore.save(function(){
-  		console.log('tore has been saved.');
-  	});
-  	res.send('lel');
-  });
-
-  router.get('/login',function(req,res){
-  	res.status(200);
-  	res.send('/login get');
+	    if (req.user) {
+	    	//User successfully authenticated
+	    	res.status(200);
+	    	res.send({status: 200});
+	    }    
   });
 
   return router;
