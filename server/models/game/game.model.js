384102ac111ee79f04b6caebe842fda54274c924
lorang92 <Steffen.ekeberg@gmail.com>
2016-04-22 10:27:13 +0200

    Updated game model to fit new design

--- server/models/game/game.model.js
+++ server/models/game/game.model.js
@@ -9,13 +9,17 @@ var gameSchema = new mongoose.Schema({
 		triples: {type: Number, default: 0, min: 0}
 	},
 	numberOfPlayers: {type: Number, default: 0},
+	canBePlaydeByMorePlayers: {type: Boolean, default: false},
+	canBePlayedInTeams: {type: Boolean, default: false},
+	otherObjects: [{type: String}],
 	rules: [{type: String}],
 	alternativeRules: [{type: String}],
 	owner: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'user'},
 	reviews: [{type: mongoose.Schema.Types.ObjectId, ref: 'review'}],
 	numberOfVotes: {type: Number, default: 0},
 	sumOfVotes: {type: Number, default: 0},
-	images: [{type: String, validate: validators.isURL()}]
+	images: [{type: String, validate: validators.isURL()}],
+	parentGame: {type: mongoose.Schema.Types.ObjectId, ref: 'game'}
 });
 
 gameSchema.index({title: 'text', mainDescription: 'text'});
