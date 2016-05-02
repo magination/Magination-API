14b8d7df107b71d87a31c558c72db9f327c0ebb8
lorang92 <Steffen.ekeberg@gmail.com>
2016-05-02 17:56:55 +0200

    made some changes to init and cronjobs due to change in how rating is stored in game.

--- server/init/init.js
+++ server/init/init.js
@@ -6,28 +6,6 @@ var crontabjobs = require('../cronjobs/cronjobs');
 var winston = require('winston');
 
 module.exports = {
-	initTopGames: function () {
-		GameList.findOne({title: 'topGames'}, function (err, list) {
-			if (err) return winston.log('error', err);
-			if (!list) {
-				// topGames does not exist. Initiate.
-				Game.find({sumOfVotes: {$gt: 0}, numberOfVotes: {$gt: 0}}, function (err, games) {
-					if (err) return winston.log('error', err);
-					games.sort(function (a, b) {
-						return parseFloat(a.sumOfVotes / a.numberOfVotes) - parseFloat(b.sumOfVotes / b.numberOfVotes);
-					});
-					var topGames = new GameList({title: 'topGames'});
-					var topGamesList = games.slice(0, 11);
-					topGamesList.forEach(function (game) {
-						topGames.games.push(game._id);
-					});
-					topGames.save(function (err) {
-						if (err) return winston.log('error', err);
-					});
-				});
-			}
-		});
-	},
 	initFeaturedGames: function () {
 		GameList.findOne({title: 'featuredGames'}, function (err, list) {
 			if (err) return winston.log('error', err);
