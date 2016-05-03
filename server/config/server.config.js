0df9be5364ed82682f68381198526ddbdb502d3a
lorang92 <Steffen.ekeberg@gmail.com>
2016-05-03 19:38:57 +0200

    added standard config file.

---
+++ server/config/server.config.js
@@ -0,0 +1,10 @@
+var server = {
+	PORT: 8000,
+	ADDRESS: 'https://localhost:',
+	SECRET: 'topSecret',
+	MAX_CONTENT_LENGTH_ACCEPTED: 999999999,
+	LOCAL_ROOT_IMAGE_PATH: 'YOURPATH',
+	ABSOLUTE_IMAGE_PATH_ROOT: 'https://localhost:8000/public/images/'
+};
+
+module.exports = server;
