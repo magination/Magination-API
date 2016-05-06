48effae9d33560ba545944f4fb3731991483078a
Simen Vie <simenvie@gmail.com>
2016-05-06 09:42:14 +0200

    rename image to img

--- server/config/server.config.js
+++ server/config/server.config.js
@@ -3,7 +3,7 @@ var server = {
 	ADDRESS: 'https://localhost:',
 	SECRET: 'topSecret',
 	MAX_CONTENT_LENGTH_ACCEPTED: 999999999,
-	LOCAL_ROOT_IMAGE_PATH: '/Users/petteriversen/WebstormProjects/MaginationApi/public/img/',
+	LOCAL_ROOT_IMAGE_PATH: '/home/simo/Projects/Node/maginationapi/public/img/',
 	ABSOLUTE_IMAGE_PATH_ROOT: 'https://localhost:8000/public/img/upload/'
 };
 
