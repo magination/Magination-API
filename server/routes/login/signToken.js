469eaaa0fda61402d9e0ab0d4f7e8ad2362e6100
lorang92 <Steffen.ekeberg@gmail.com>
2016-05-09 12:37:02 +0200

    signToken now signes exp trough the options fild in jwt.sign(payload,secret,options)

--- server/routes/login/signToken.js
+++ server/routes/login/signToken.js
@@ -5,16 +5,14 @@ module.exports = function (req, res, next) {
 	var token = jwt.sign({
 		id: req.user.id,
 		userVersion: req.user.userVersion,
-		privileges: req.user.privileges,
-		expiresIn: 60 * 60
-	}, serverConfig.SECRET);
+		privileges: req.user.privileges
+	}, serverConfig.SECRET, {expiresIn: (60 * 60)});
 
 	var refreshToken = jwt.sign({
 		id: req.user.id,
 		userVersion: req.user.userVersion,
-		privileges: req.user.privileges,
-		expiresIn: 60 * 60 * 24 * 60
-	}, serverConfig.SECRET);
+		privileges: req.user.privileges
+	}, serverConfig.SECRET, {expiresIn: (60 * 60 * 24 * 60)});
 
 	var data = {
 		token: token,
