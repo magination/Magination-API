3bfa99ccef326cd0efbe21cf0dc4a8487d19db38
lorang92 <Steffen.ekeberg@gmail.com>
2016-05-09 10:22:57 +0200

    signToken now signs with a refreshToken. Updated some http-responsens in refreshToken.js

--- server/routes/login/refreshToken.js
+++ server/routes/login/refreshToken.js
@@ -6,10 +6,10 @@ var signToken = require('./signToken');
 
 module.exports = function (req, res, next) {
 	User.findById(req.verified.id, function (err, user) {
-		if (err) return res.status(500).json({message: 'internal server error'});
-		if (!user) return res.status(404).json({message: 'user could not be found'});
-		if (isNaN(req.verified.userVersion) || isNaN(user.userVersion)) return res.status(400).json({message: constants.httpResponseMessages.badRequest});
-		if (parseInt(req.verified.userVersion) !== parseInt(user.userVersion)) return res.status(403).json({message: constants.httpResponseMessages.forbidden});
+		if (err) return res.status(500).send();
+		if (!user) return res.status(404).send();
+		if (isNaN(req.verified.userVersion) || isNaN(user.userVersion)) return res.status(500).send();
+		if (parseInt(req.verified.userVersion) !== parseInt(user.userVersion)) return res.status(403).send();
 		req.user = user;
 		signToken(req, res, next);
 	});
--- server/routes/login/signToken.js
+++ server/routes/login/signToken.js
@@ -6,13 +6,21 @@ module.exports = function (req, res, next) {
 		id: req.user.id,
 		userVersion: req.user.userVersion,
 		privileges: req.user.privileges,
-		expiresIn: 60 * 60 * 12
+		expiresIn: 60 * 60
+	}, serverConfig.SECRET);
+
+	var refreshToken = jwt.sign({
+		id: req.user.id,
+		userVersion: req.user.userVersion,
+		privileges: req.user.privileges,
+		expiresIn: 60 * 60 * 24 * 60
 	}, serverConfig.SECRET);
 
 	var data = {
 		token: token,
+		refreshToken: refreshToken,
 		id: req.user.id,
-		expiresIn: Date.now() + (60 * 60 * 12 * 1000)
+		expiresIn: Date.now() + (60 * 60 * 1000)
 	};
 
 	req.data = data;
