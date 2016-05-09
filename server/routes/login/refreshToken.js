eeff05e9cdd5f4417d532c74006f9e94ad9eb225
lorang92 <Steffen.ekeberg@gmail.com>
2016-05-09 13:57:03 +0200

    refrehToken now returns 401 instead of 403 if userVerion is different.

--- server/routes/login/refreshToken.js
+++ server/routes/login/refreshToken.js
@@ -9,7 +9,7 @@ module.exports = function (req, res, next) {
 		if (err) return res.status(500).send();
 		if (!user) return res.status(404).send();
 		if (isNaN(req.verified.userVersion) || isNaN(user.userVersion)) return res.status(500).send();
-		if (parseInt(req.verified.userVersion) !== parseInt(user.userVersion)) return res.status(403).send();
+		if (parseInt(req.verified.userVersion) !== parseInt(user.userVersion)) return res.status(401).send();
 		req.user = user;
 		signToken(req, res, next);
 	});
