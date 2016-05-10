6697710184689bef197555454a26148e3c770c8e
lorang92 <Steffen.ekeberg@gmail.com>
2016-05-10 21:56:32 +0200

    Moved REMOTE_GAME_SITE url to config. Used inn all emails sent.

--- server/config/server.config.js
+++ server/config/server.config.js
@@ -4,7 +4,8 @@ var server = {
 	SECRET: 'topSecret',
 	MAX_CONTENT_LENGTH_ACCEPTED: 999999999,
 	LOCAL_ROOT_IMAGE_PATH: 'C:/Users/Steffen/Desktop/Maginationapiworkdraft/public/img/',
-	ABSOLUTE_IMAGE_PATH_ROOT: 'https://localhost:8000/public/img/upload/'
+	ABSOLUTE_IMAGE_PATH_ROOT: 'https://localhost:8000/public/img/upload/',
+	REMOTE_GAME_SITE: 'http://localhost:8080'
 };
 
 module.exports = server;
