2a36ce17d94e54eb5e42cb1902df8dddbeab3887
Simen Vie <simenvie@gmail.com>
2016-05-11 10:38:03 +0200

    dynamic image path

--- server/config/server.config.js
+++ server/config/server.config.js
@@ -1,9 +1,10 @@
+var path = require('path');
 var server = {
 	PORT: 8000,
 	ADDRESS: 'https://localhost:',
 	SECRET: 'topSecret',
 	MAX_CONTENT_LENGTH_ACCEPTED: 999999999,
-	LOCAL_ROOT_IMAGE_PATH: 'C:/Users/Steffen/Desktop/Maginationapiworkdraft/public/img/',
+	LOCAL_ROOT_IMAGE_PATH: path.join(__dirname, '../../public/img'),
 	ABSOLUTE_IMAGE_PATH_ROOT: 'https://localhost:8000/public/img/upload/',
 	REMOTE_GAME_SITE: 'http://localhost:8080'
 };
