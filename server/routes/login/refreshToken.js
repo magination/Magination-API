3ee9a2528e1ca095804c3f8c0c6ad549e184c50b
lorang92 <Steffen.ekeberg@gmail.com>
2016-05-11 13:10:26 +0200

    added isBanned as a field in user. This is now checked when you log in and use the refreshToken.

--- server/routes/login/refreshToken.js
+++ server/routes/login/refreshToken.js
@@ -10,6 +10,7 @@ module.exports = function (req, res, next) {
 		if (!user) return res.status(404).send();
 		if (isNaN(req.verified.userVersion) || isNaN(user.userVersion)) return res.status(500).send();
 		if (parseInt(req.verified.userVersion) !== parseInt(user.userVersion)) return res.status(401).send();
+		if (user.isBanned) return res.status(401).send();
 		req.user = user;
 		signToken(req, res, next);
 	});
