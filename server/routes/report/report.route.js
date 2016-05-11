519b0b81206413454e681526c824979867357069
lorang92 <Steffen.ekeberg@gmail.com>
2016-05-11 10:48:19 +0200

    Changes made to how reports are posted and deleted. Added tests.

--- server/routes/report/report.route.js
+++ server/routes/report/report.route.js
@@ -13,33 +13,30 @@ var userBruteForce = require('../../bruteforce/bruteForce').userBruteForce;
 
 module.exports = function (app) {
 	var validatePostRequest = function (req, res, next) {
-		if (!req.body.reportText || !req.body.type || !req.body.id) return res.status(422).json({message: constants.httpResponseMessages.unprocessableEntity});
+		if (!req.body.reportText || !req.body.id) return res.status(422).json({message: constants.httpResponseMessages.unprocessableEntity});
 		if (!validator.isValidId(req.body.id)) return res.status(422).json({message: constants.httpResponseMessages.unprocessableEntity});
 		next();
 	};
 
 	var checkIfIdExists = function (req, res, next) {
-		if (req.body.type === 'game') {
+		if (req.body.type === Report.types.GAME) {
 			Game.findById({_id: req.body.id}, function (err, game) {
 				if (err) return res.status(500).json({message: constants.httpResponseMessages.internalServerError});
 				if (!game) return res.status(404).json({message: constants.httpResponseMessages.notFound});
-				req.body.type = Report.types.GAME;
 				next();
 			});
 		}
-		else if (req.body.type === 'review') {
+		else if (req.body.type === Report.types.REVIEW) {
 			Review.findById({_id: req.body.id}, function (err, review) {
 				if (err) return res.status(500).json({message: constants.httpResponseMessages.internalServerError});
 				if (!review) return res.status(404).json({message: constants.httpResponseMessages.notFound});
-				req.body.type = Report.types.REVIEW;
 				next();
 			});
 		}
-		else if (req.body.type === 'user') {
+		else if (req.body.type === Report.types.USER) {
 			User.findById({_id: req.body.id}, function (err, user) {
 				if (err) return res.status(500).json({message: constants.httpResponseMessages.internalServerError});
 				if (!user) return res.status(404).json({message: constants.httpResponseMessages.notFound});
-				req.body.type = Report.types.USER;
 				next();
 			});
 		}
@@ -52,47 +49,135 @@ module.exports = function (app) {
 		else return res.status(401).json({message: constants.httpResponseMessages.unauthorized});
 	};
 
-	router.post('/reports', validatePostRequest, verifyToken, checkIfIdExists, function (req, res) {
-		var newReport = new Report({reportText: req.body.reportText, type: req.body.type, id: req.body.id, owner: req.verified.id});
-		newReport.save(function (err) {
-			if (err) return res.status(500).json({mesage: constants.httpResponseMessages.internalServerError});
-			else return res.status(201).json(newReport);
+	router.post('/reports/games', validatePostRequest, verifyToken, function (req, res) {
+		req.body.type = Report.types.GAME;
+		checkIfIdExists(req, res, function () {
+			Report.findOne({owner: req.verified.id, id: req.body.id, type: req.body.type}, function (err, report) {
+				if (err) return res.status(500).send();
+				if (report) return res.status(409).send();
+				var newReport = new Report({reportText: req.body.reportText, type: req.body.type, id: req.body.id, owner: req.verified.id});
+				newReport.save(function (err) {
+					if (err) return res.status(500).json({mesage: constants.httpResponseMessages.internalServerError});
+					else return res.status(201).json(newReport);
+				});
+			});
 		});
 	});
 
-	router.get('/reports', verifyToken, verifyPrivileges, function (req, res) {
-		Report.find({}, function (err, reports) {
-			if (err) return res.status(500).json({mesage: constants.httpResponseMessages.internalServerError});
-			else return res.status(200).json(reports);
+	router.post('/reports/users', validatePostRequest, verifyToken, function (req, res) {
+		req.body.type = Report.types.USER;
+		checkIfIdExists(req, res, function () {
+			Report.findOne({owner: req.verified.id, id: req.body.id, type: req.body.type}, function (err, report) {
+				if (err) return res.status(500).send();
+				if (report) return res.status(409).send();
+				var newReport = new Report({reportText: req.body.reportText, type: req.body.type, id: req.body.id, owner: req.verified.id});
+				newReport.save(function (err) {
+					if (err) return res.status(500).json({mesage: constants.httpResponseMessages.internalServerError});
+					else return res.status(201).json(newReport);
+				});
+			});
 		});
 	});
+
+	router.post('/reports/reviews', validatePostRequest, verifyToken, function (req, res) {
+		req.body.type = Report.types.REVIEW;
+		checkIfIdExists(req, res, function () {
+			Report.findOne({owner: req.verified.id, id: req.body.id, type: req.body.type}, function (err, report) {
+				if (err) return res.status(500).send();
+				if (report) return res.status(409).send();
+				var newReport = new Report({reportText: req.body.reportText, type: req.body.type, id: req.body.id, owner: req.verified.id});
+				newReport.save(function (err) {
+					if (err) return res.status(500).json({mesage: constants.httpResponseMessages.internalServerError});
+					else return res.status(201).json(newReport);
+				});
+			});
+		});
+	});
+
+	var findIndex = function (array, id) {
+		for (var i = 0; i < array.length; i++) {
+			if (array[i].id === id) return i;
+		}
+		return -1;
+	};
+
+	var containsId = function (array, id) {
+		for (var i = 0; i < array.length; i++) {
+			if (array[i].id === id) return true;
+		}
+		return false;
+	};
+
+	var orderById = function (array) {
+		var data = [];
+		array.forEach(function (e) {
+			var id = e.id;
+			var obj = {id: id, reports: []};
+			var index = findIndex(data, id);
+			if (index < 0) data.push(obj);
+			index = findIndex(data, id);
+			data[index].reports.push(e);
+		});
+		return data;
+	};
 
 	router.get('/reports/games', verifyToken, verifyPrivileges, function (req, res) {
+		var data = {
+			games: []
+		};
 		Report.find({type: Report.types.GAME}, function (err, reports) {
 			if (err) return res.status(500).json({mesage: constants.httpResponseMessages.internalServerError});
-			else return res.status(200).json(reports);
+			data.games = orderById(reports);
+			return res.status(200).json(data);
 		});
 	});
 
 	router.get('/reports/users', verifyToken, verifyPrivileges, function (req, res) {
+		var data = {
+			users: []
+		};
 		Report.find({type: Report.types.USER}, function (err, reports) {
 			if (err) return res.status(500).json({mesage: constants.httpResponseMessages.internalServerError});
-			else return res.status(200).json(reports);
+			data.users = orderById(reports);
+			return res.status(200).json(data);
 		});
 	});
 
 	router.get('/reports/reviews', verifyToken, verifyPrivileges, function (req, res) {
+		var data = {
+			reviews: []
+		};
 		Report.find({type: Report.types.REVIEW}, function (err, reports) {
 			if (err) return res.status(500).json({mesage: constants.httpResponseMessages.internalServerError});
-			else return res.status(200).json(reports);
+			data.reviews = orderById(reports);
+			return res.status(200).json(data);
 		});
 	});
 
-	router.delete('/reports/:id', verifyToken, verifyPrivileges, function (req, res) {
-		if (!validator.isValidId(req.params.id)) return res.status(422).json({message: constants.httpResponseMessages.unprocessableEntity});
-		Report.remove({_id: req.params.id}, function (err) {
-			if (err) return res.status(500).json({mesage: constants.httpResponseMessages.internalServerError});
-			return res.status(204).json({message: constants.httpResponseMessages.deleted});
+	router.delete('/reports/games/:id', verifyToken, verifyPrivileges, function (req, res) {
+		if (!validator.isValidId(req.params.id)) return res.status(404).send();
+		Report.remove({type: Report.types.GAME, id: req.params.id}, function (err, obj) {
+			if (err) return res.status(500).send();
+			if (obj.result.n === 0) return res.status(404).send();
+			return res.status(204).send();
+		});
+	});
+
+	router.delete('/reports/users/:id', verifyToken, verifyPrivileges, function (req, res) {
+		if (!validator.isValidId(req.params.id)) return res.status(404).send();
+		Report.remove({type: Report.types.USER, id: req.params.id}, function (err, obj) {
+			if (err) return res.status(500).send();
+			if (obj.result.n === 0) return res.status(404).send();
+			return res.status(204).send();
+		});
+	});
+
+	router.delete('/reports/reviews/:id', verifyToken, verifyPrivileges, function (req, res) {
+		if (!validator.isValidId(req.params.id)) return res.status(404).send();
+		Report.remove({type: Report.types.REVIEW, id: req.params.id}, function (err, obj) {
+			if (err) return res.status(500).send();
+			if (obj.result.n === 0) return res.status(404).send();
+			return res.status(204).send();
 		});
 	});
 
