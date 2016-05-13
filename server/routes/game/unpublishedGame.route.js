f8f97af66ac0f85cdc07c1fc6dd43dbc931d298e
lorang92 <Steffen.ekeberg@gmail.com>
2016-05-13 13:33:37 +0200

    updated to now logg errors through the logger.

--- server/routes/game/unpublishedGame.route.js
+++ server/routes/game/unpublishedGame.route.js
@@ -6,12 +6,13 @@ var verifyToken = require('../login/verifyToken');
 var validator = require('../../validator/validator');
 var constants = require('../../config/constants.config');
 var _ = require('lodash');
+var logger = require('../../logger/logger');
 var Report = require('../../models/report/report.model');
 var winston = require('winston');
 
 module.exports = function (app) {
 	var verifyPostRequest = function (req, res, next) {
-		if (req.body._id) return res.status(422).json({message: 'A unpublishedGame can not be created with a given _id.'});
+		if (req.body._id) return res.status(422).json({message: 'An unpublishedGame can not be created with a given _id.'});
 		next();
 	};
 
@@ -19,42 +20,60 @@ module.exports = function (app) {
 		var unpublishedGame = new UnpublishedGame(_.extend(req.body, {owner: req.verified.id}));
 		if (!req.body.parentGame) unpublishedGame.parentGame = undefined;
 		unpublishedGame.save(function (err) {
-			if (err) return res.status(500).json({message: constants.httpResponseMessages.internalServerError});
+			if (err) {
+				logger.log('error', 'POST /unpublishedGames', err);
+				return res.status(500).send();
+			}
 			unpublishedGame.populate('owner', 'username', function (err) {
-				if (err) return res.status(500).json({message: constants.httpResponseMessages.internalServerError});
+				if (err) {
+					logger.log('error', 'POST /unpublishedGames', err);
+					return res.status(500).send();
+				}
 				else return res.status(201).json(unpublishedGame);
 			});
 		});
 	});
 
 	router.get('/users/:userId/unpublishedGames', verifyToken, function (req, res) {
-		if (!validator.isValidId(req.params.userId)) return res.status(422).json({message: constants.httpResponseMessages.unprocessableEntity});
-		if (req.verified.id !== req.params.userId) return res.status(401).json({message: constants.httpResponseMessages.forbidden});
+		if (!validator.isValidId(req.params.userId)) return res.status(422).send();
+		if (req.verified.id !== req.params.userId) return res.status(401).send();
 		UnpublishedGame.find({owner: req.verified.id}, function (err, games) {
-			if (err) return res.status(500).json({message: constants.httpResponseMessages.internalServerError});
+			if (err) {
+				logger.log('error', 'GET /users/:userId/unpublishedGames', err);
+				return res.status(500).send();
+			}
 			else return res.status(200).json(games);
 		});
 	});
 
 	router.put('/unpublishedGames/:id', verifyToken, function (req, res) {
-		if (!validator.isValidId(req.params.id)) return res.status(422).json({message: constants.httpResponseMessages.unprocessableEntity});
+		if (!validator.isValidId(req.params.id)) return res.status(422).send();
 		if (req.body.parentGame === '') req.body.parentGame = undefined;
 		UnpublishedGame.findOneAndUpdate({_id: req.params.id, owner: req.verified.id}, req.body, {new: true}, function (err, game) {
-			if (err) return res.status(500).json({message: constants.httpResponseMessages.internalServerError});
-			if (!game) return res.status(404).json({message: constants.httpResponseMessages.notFound});
-			return res.status(200).json(game);
+			if (err) {
+				logger.log('error', 'PUT /unpublishedGames/:id', err);
+				return res.status(500).send();
+			}
+			else if (!game) return res.status(404).send();
+			else return res.status(200).json(game);
 		});
 	});
 
 	router.delete('/unpublishedGames/:id', verifyToken, function (req, res) {
-		if (!validator.isValidId(req.params.id)) return res.status(422).json({message: constants.httpResponseMessages.unprocessableEntity});
+		if (!validator.isValidId(req.params.id)) return res.status(422).send();
 		UnpublishedGame.findById({_id: req.params.id}, function (err, game) {
-			if (err) return res.status(500).json({message: constants.httpResponseMessages.internalServerError});
-			if (!game) return res.status(404).json({message: constants.httpResponseMessages.notFound});
-			if (!game.owner.equals(req.verified.id)) return res.status(401).json({message: constants.httpResponseMessages.unauthorized});
+			if (err) {
+				logger.log('error', 'DELETE /unbpublishedGames/:id', err);
+				return res.status(500).send();
+			}
+			if (!game) return res.status(404).send();
+			if (!game.owner.equals(req.verified.id)) return res.status(401).send();
 			UnpublishedGame.remove({_id: req.params.id}, function (err, game) {
-				if (!game) return res.status(404).json({message: constants.httpResponseMessages.notFound});
-				if (err) return res.status(500).json({message: constants.httpResponseMessages.internalServerError});
+				if (err) {
+					logger.log('error', 'DELETE /unpublishedGames/:id', err);
+					return res.status(500).send();
+				}
+				else if (!game) return res.status(404).send();
 				else res.status(200).json();
 			});
 		});
@@ -70,11 +89,17 @@ module.exports = function (app) {
 			game.publishGame(function (err, publishedGame) {
 				if (err) {
 					if (err.code === 11000) return res.status(409).send(); // duplicate key. Title is in use.
-					return res.status(500).send();
+					else {
+						logger.log('error', 'POST /unpublishedGames/:id/publish', err);
+						return res.status(500).send();
+					}
 				}
-				if (!publishedGame) return res.status(404).json({message: constants.httpResponseMessages.notFound});
+				if (!publishedGame) return res.status(404).send();
 				UnpublishedGame.findByIdAndRemove({_id: game._id}, function (err) {
-					if (err) return res.status(500).json({message: constants.httpResponseMessages.internalServerError});
+					if (err) {
+						logger.log('error', 'POST /unpublishedGames/:id/publish', err);
+						return res.status(500).send();
+					}
 					else {
 						moveReportsFromUnpublishedToPublishedGame(game, publishedGame);
 						return res.status(200).json(publishedGame);
