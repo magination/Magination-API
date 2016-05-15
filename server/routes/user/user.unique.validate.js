903a7048496b3c0d213bad6f4ff441214703ddf9
lorang92 <Steffen.ekeberg@gmail.com>
2016-05-15 14:21:48 +0200

    unqique.validator now checks on lowercase.

--- server/routes/user/user.unique.validate.js
+++ server/routes/user/user.unique.validate.js
@@ -2,7 +2,7 @@
 var User = require('../../models/user/user.model');
 
 module.exports = function (req, res, next) {
-	User.findOne({$or: [{username: req.body.username}, {email: req.body.email}]}, function (err, user) {
+	User.findOne({$or: [{username: req.body.username.toLowerCase()}, {email: req.body.email.toLowerCase()}]}, function (err, user) {
 		if (err) return res.status(500).json({message: 'Internal server error'});
 		if (user) {
 			if (user.username === req.body.username) return res.status(409).json({message: 'Username already exists'});
